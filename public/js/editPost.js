53cb06d623eddfcd8ed96d4e5b67394c768e9e28
Sidd <siddz415@gmail.com>
2023-04-06 15:59:32 -0700

    added funtionality to editpost

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,15 +1,15 @@
 async function editFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value;
+    const title = document.querySelector('input[name="project-name"]').value;
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
       ];
 
-    const response = await fetch(`/api/posts/${id}`, {
+    const response = await fetch(`/api/projects/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
-            title   // ask how to change this
+            title  // ask how to change this
         }),
         headers: {
             'Content-Type': 'application/json'
