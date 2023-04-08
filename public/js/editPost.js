4ae7d8dcf3fe91c1ae20e9539d94fb3f66c089e8
Sidd <siddz415@gmail.com>
2023-04-07 21:41:17 -0700

    added input

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,12 +1,13 @@
 async function editFormHandler(event) {
     event.preventDefault();
 
-    const title = document.querySelector('input[name="project-name"]').value;
+    const title = document.querySelector('input[name="post-name"]').value;
+    const description = document.querySelector('textarea[name="post-desc"]').value;
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
       ];
 
-    const response = await fetch(`/api/projects/${id}`, {
+    const response = await fetch(`/api/edit/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
             title ,
